2bf28314254ba907164f2ee1e3444ba75580bfd4
melivaldiviezo <valdiviezomelina@gmail.com>
2022-12-08 19:57:44 -0300

    form change

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import useInput from './utils/useInput'
 
 
 const Form = () => {
 const name = useInput('text','name', "name")
 const email = useInput('email','email', "email")
+const [msg, setMsg] = useState();
+const validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)
+const validName = /^.{5,}$/.test(name.value)
+console.log(name.value);
+
+const onClick = (e) =>{
+  e.preventDefault();
+  if(validEmail && validName){
+    setMsg(`Thanks ${name.value} our team will contact you soon!`)
+  } else{
+    setMsg('Error pelotuda')
+  }
+}
   return (
-    <div>
+    <div className="form">
       <form>
         <input {...name} />
         <input {...email} />
-        <input type='submit' value="send"/>
+        <input type='submit' value="send" onClick={onClick}/>
+        <p>{msg}</p>
       </form>
     </div>
   );
--- src/Components/utils/useInput.jsx
+++ src/Components/utils/useInput.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const useInput = (type, name, placeholder) => {
     const [value, setValue] = useState()
     const onChange = (e) =>{
         setValue(e.target.value)
     }
-    console.log(value)
 return {
     value,
     onChange,
